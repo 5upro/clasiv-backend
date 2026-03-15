f07537fb5c0da0c09aaacf52fc65ce1eb82e5087
SUPRØ <00youareworngtho00@gmail.com>
2026-03-15 08:23:54 +0530

    feat(express): add pagination field to req obj

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -3,11 +3,13 @@ import {
 	RefreshTokenPayload, 
 	AccessTokenPayload, 
 } from "./auth";
+import { Pagination } from "@/types/httpQuery";
 
 declare global {
     namespace Express {
         interface Request {
             user?: RefreshTokenPayload | AccessTokenPayload;
+			pagination?: Pagination;
 		}
     }
 }
