a43371ef735d38eb9fc9ce87eedaa43e698268e4
SUPRØ <00youareworngtho00@gmail.com>
2026-04-16 03:06:24 +0530

    chore: remove migrations directory

--- src/migrations/departments.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS departments (
-	id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-	name TEXT NOT NULL UNIQUE,
-	abbrv TEXT NOT NULL UNIQUE
-);
--- src/migrations/role_permissions.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS role_permissions (
-	role_id SMALLINT NOT NULL REFERENCES roles(id),
-	permission_id SMALLINT NOT NULL REFERENCES permissions(id),
-	PRIMARY KEY (role_id, permission_id)
-);
