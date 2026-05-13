30a0205bb0c6a07063294cf1df28f5d61ad1bc32
SUPRØ <00youareworngtho00@gmail.com>
2026-05-13 21:42:33 +0530

    docs(modules/assignments): add documentation for routes

--- src/modules/assignments/assignments.routes.ts
+++ src/modules/assignments/assignments.routes.ts
@@ -11,16 +11,46 @@ const router = Router();
 
 router.use(authentication);
 
+/*
+* ROUTE: POST /assignments
+* BODY: 
+* {
+*     title: string,
+*     description: string,
+*     collegeCourseSubjectId: string,
+*     maxMarks?: number,
+*     attachmentUrl?: string,
+*     dueAt: Date, [in string format]
+*     expiresAt: Date, [in string format]
+*     filePattern: number[],
+* }
+*/
 router.post("/", 
     validator(CreateAssignmentSchema),
     assignmentController.createAssignment
 );
+
+/*
+* ROUTE: GET /assignments
+*/
 router.get("/", 
     assignmentController.getAssignments
 );
+
+/*
+* ROUTE: GET /assignments/:id
+*/
 router.get("/:id", 
     assignmentController.getAssignment
 );
+
+/*
+* ROUTE: POST /assignments
+* BODY: 
+* {
+*     fileSize: number,
+* }
+*/
 router.post("/:id/submissions",
     validator(UploadSubmissionSchema),
 	assignmentController.createSubmission
