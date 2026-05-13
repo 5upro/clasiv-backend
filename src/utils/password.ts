bf56d72507b2388f9d54115829bf155ad6190c67
SUPRØ <00youareworngtho00@gmail.com>
2026-05-13 18:11:12 +0530

    refactor(utils): change argon2 default params

--- src/utils/password.ts
+++ src/utils/password.ts
@@ -1,7 +1,11 @@
 import argon2 from 'argon2';
 
 export const hashPassword = async (password: string): Promise<string> => {
-	return await argon2.hash(password);
+	return await argon2.hash(password,{
+        memoryCost: 8192, 
+        timeCost: 2, 
+        parallelism: 1, 
+    });
 };
 
 export const verifyPassword = async (password: string, hash: string): Promise<boolean> => {
